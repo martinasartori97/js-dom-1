6d447d6c44d950eb4dc7f504bfb498e22787f15a
martinasartori97 <martinasartori9@gmail.com>
2024-10-08 16:58:47 +0200

    edit js

--- img/main.js
+++ img/main.js
@@ -14,15 +14,15 @@ console.log(button);
 
 function accendiLampadina() {
     const lampadina = document.getElementById("lampadina");
-    lampadina.src = "img\yellow_lamp.png"
+    lampadina.src = "yellow_lamp.png"
 
-    if (lampadina.src.includes("img\white_lamp.png")) {
-        lampadina.src = "img\yellow_lamp.png";
-        button.textcontent = "spegni";
-    } else {
-        lampadina.src = "img\white_lamp.png"
-        button.textcontent = "accendi";
-    }
+   // if (lampadina.src.includes("img\white_lamp.png")) {
+       // lampadina.src = "img\yellow_lamp.png";
+        //button.textcontent = "spegni";
+   // } else {
+        //lampadina.src = "img\white_lamp.png"
+       // button.textcontent = "accendi";
+    //}
 
 }
 console.log(lampadina);
