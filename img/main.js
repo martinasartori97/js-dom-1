412f1b60b53ca3645459b574077320d88c3e30b1
martinasartori97 <martinasartori9@gmail.com>
2024-10-08 14:51:33 +0200

    edit js

--- img/main.js
+++ img/main.js
@@ -1,9 +1,15 @@
 console.log("hello");
 
-function accendiLampadina(){
-const lampadina = document.getElementById("lampadina");
-lampadina.src = img/yellow_lamp.png
+
+
+function accendiLampadina() {
+    const lampadina = document.getElementById("lampadina");
+    lampadina.src = img / yellow_lamp.png
 }
 
-const button = document.getElementById("accendiButton");
-button.addEventListener("click", accendiLampadina);
+
+function clickAccendi() {
+    const button = document.getElementById("accendiButton");
+    button.addEventListener("click", accendiLampadina);
+}
+
