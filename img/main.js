ebf003ed4a35d587bc69a792b4b0ae250c07a9c8
martinasartori97 <martinasartori9@gmail.com>
2024-10-08 15:07:20 +0200

    edit js

--- img/main.js
+++ img/main.js
@@ -1,15 +1,20 @@
 console.log("hello");
 
+function clickAccendi() {
+    const button = document.getElementById("button");
+    button.addEventListener("click", accendiLampadina);
 
+}
+
+
+function aggiungiImmagine() {
+    const nuovaImmagine = document.createElement("img")
+    nuovaImmagine.src = img\yellow_lamp.png
+}
 
 function accendiLampadina() {
     const lampadina = document.getElementById("lampadina");
-    lampadina.src = img / yellow_lamp.png
-}
+    lampadina.src = img\yellow_lamp.png
 
 
-function clickAccendi() {
-    const button = document.getElementById("accendiButton");
-    button.addEventListener("click", accendiLampadina);
-}
 
