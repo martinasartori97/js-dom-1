35d009cb45094ebd082e72542189c31dc297e10a
martinasartori97 <martinasartori9@gmail.com>
2024-10-08 15:51:50 +0200

    edit js

--- img/main.js
+++ img/main.js
@@ -13,7 +13,6 @@ function accendiLampadina() {
     const lampadina = document.getElementById("lampadina");
     lampadina.src = "img\yellow_lamp.png"
 }
-accendiLampadina();
 console.log(lampadina);
 
 
