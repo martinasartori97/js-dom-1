14353a453698c543a630afe2214e5226bb2e6c82
martinasartori97 <martinasartori9@gmail.com>
2024-10-08 15:14:37 +0200

    edit js

--- img/main.js
+++ img/main.js
@@ -5,16 +5,23 @@ function clickAccendi() {
     button.addEventListener("click", accendiLampadina);
 
 }
+console.log(button);
+
+
 
 
 function aggiungiImmagine() {
     const nuovaImmagine = document.createElement("img")
     nuovaImmagine.src = img\yellow_lamp.png
 }
+console.log(nuovaImmagine);
+
 
 function accendiLampadina() {
     const lampadina = document.getElementById("lampadina");
     lampadina.src = img\yellow_lamp.png
+
+
 
 
 
